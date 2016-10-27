4aa6e6201a4bcce95aa93d573cf3f6a804b08b62
kj1352 <nikhilkj24@gmail.com>
2016-10-28 04:17:58 +0530

    first one

---
+++ js/home.js
@@ -0,0 +1,14 @@
+/*global $*/
+$(document).ready(function () {
+    "use strict";
+    $("#owl-demo").owlCarousel({
+        autoPlay : 3000,
+        stopOnHover : true,
+        navigation : true,
+        paginationSpeed : 1000,
+        goToFirstSpeed : 2000,
+        singleItem : true,
+        autoHeight : true,
+        transitionStyle : "fade"
+    });
+});
